c96d51261be824c2c80eb10e45e514c672d58e6f
cmj <819953059@qq.com>
2020-06-28 17:35:16 +0800

    提交

---
+++ src/views/jf/staff/components/ADetail/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="a-detail">
+        <div class="sum-box">
+            <div class="top">
+                <div class="sum-item-small" v-for="(value, key, index) in sumItemSmall" :key="index">
+                    <d2-icon name="bar-chart"/>
+                    <span>{{key}}</span>
+                    <span>{{value}}</span>
+                </div>
+            </div>
+            <div class="bottom">
+                <div class="sum-item-large"></div>
+            </div>
+        </div>
+        <div class="activities-box"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'a-detail',
+  data () {
+    return {
+      sumItemSmall: {
+        nowA: 0,
+        totalA: 40,
+        canChangeB: 40,
+        totalChangeB: 100,
+        fixB: 2167
+      }
+    }
+  }
+}
+</script>
--- src/views/jf/staff/index/index.vue
+++ src/views/jf/staff/index/index.vue
@@ -1,5 +1,30 @@
 <template>
     <d2-container class="page">
-        积分看板内容
+        <el-tabs>
+            <el-tab-pane label="积分看板">
+                <a-detail/>
+            </el-tab-pane>
+            <el-tab-pane label="A分明细">
+                A分明细组件
+            </el-tab-pane>
+            <el-tab-pane label="B管理积分明细">
+                B管理积分明细组件
+            </el-tab-pane>
+            <el-tab-pane label="B固定积分">
+                B固定积分
+            </el-tab-pane>
+            <el-tab-pane label="积分活动">
+                积分活动
+            </el-tab-pane>
+        </el-tabs>
     </d2-container>
 </template>
+
+<script>
+import ADetail from '../components/ADetail'
+export default {
+  components: {
+    ADetail
+  }
+}
+</script>
